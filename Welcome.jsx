320011aeb202c05a8a60de362c2adddfc48f1c33
Avinash Kumar <91388079+Avijr272@users.noreply.github.com>
2023-01-19 22:24:23 +0530

    Add files via upload

---
+++ Welcome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Welcome = ({ name, regNo }) => {
+  return (
+    <div>
+      <h1>Hey! {name}</h1>
+      <h2>Welcome to Lovely Profesional University</h2>
+      <h2>You are doing great from your side 😊😊 </h2>
+    </div>
+  );
+};
+
+
+export default Welcome;
